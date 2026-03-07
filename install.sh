8daa237992e1469e0221d157c33bc8043d723841
eternal <d0gjjylol@gmail.com>
2026-03-07 12:48:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,14 @@ echo installing packages...
 echo
 sudo pacman -S --needed git fish kitty i3 polybar feh rofi picom autotiling
 
-echo changing shell to fish
-echo
-chsh -s /usr/bin/fish
-
-# create config folders
 mkdir -p ~/.config/{picom,i3,kitty,polybar,rofi}
 
-# copy configs
 cp picom.conf ~/.config/picom/picom.conf
 cp config ~/.config/i3/config
 cp config.ini ~/.config/polybar/config.ini
 cp config.rasi ~/.config/rofi/config.rasi
 cp kitty.conf ~/.config/kitty/kitty.conf
+
+echo changing shell to fish
+echo
+chsh -s /usr/bin/fish
