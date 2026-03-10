ce31f437ddd0e06cf74bba3393df1bc0233c8807
eternal <d0gjjylol@gmail.com>
2026-03-10 17:45:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+set -e
 
-echo installing packages...
+trap 'echo; echo "installation failed on command: $BASH_COMMAND"' ERR
+
+echo "installing packages..."
 echo
-sudo pacman -S --needed git fish kitty i3 polybar feh rofi picom autotiling maim slop xclip
+
+sudo pacman -S --needed --noconfirm git fish kitty i3 polybar feh rofi picom autotiling maim slop xclip
 
 mkdir -p ~/.config/{picom,i3,kitty,polybar,rofi}
 
-cp picom.conf ~/.config/picom/picom.conf
-cp config ~/.config/i3/config
-cp config.ini ~/.config/polybar/config.ini
-cp config.rasi ~/.config/rofi/config.rasi
-cp kitty.conf ~/.config/kitty/kitty.conf
+cp ~/blackframe/picom.conf ~/.config/picom/picom.conf
+cp ~/blackframe/config ~/.config/i3/config
+cp ~/blackframe/config.ini ~/.config/polybar/config.ini
+cp ~/blackframe/config.rasi ~/.config/rofi/config.rasi
+cp ~/blackframe/kitty.conf ~/.config/kitty/kitty.conf
 
-echo changing shell to fish
+echo "changing shell to fish"
 echo
 chsh -s /usr/bin/fish
+
+echo
+echo "installation complete. reboot."
